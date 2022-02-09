cac7e664bb49b1384d27533e8a752111291a5bb6
William <william@famosos.com>
2022-02-09 11:10:47 -0500

    CheckBox and RadioButton

--- src/com/company/views/home/JPanelHome.java
+++ src/com/company/views/home/JPanelHome.java
@@ -4,12 +4,59 @@ import javax.swing.*;
 
 public class JPanelHome extends JPanel {
 
-    private  JLabel jLabelTitle;
+    private JLabel jLabelTitle;
+
+    private JCheckBox checkBoxAcceptTerm;
+    private JCheckBox checkBoxAcceptTerm1;
+    private JCheckBox checkBoxAcceptTerm2;
+
+    private ButtonGroup buttonGroup;
+    private JRadioButton jRadioButtonOption1;
+    private JRadioButton jRadioButtonOption2;
+    private JRadioButton jRadioButtonOption3;
+
+
     public JPanelHome() {
         this.setLayout(null);
 
         jLabelTitle = new JLabel("Home");
-        jLabelTitle.setBounds(10, 10, 100,30);
+        jLabelTitle.setBounds(10, 10, 100, 30);
         this.add(jLabelTitle);
+
+        checkBoxAcceptTerm = new JCheckBox("Acepto términos y condiciones");
+        checkBoxAcceptTerm.setBounds(10, 50, 200, 30);
+        checkBoxAcceptTerm.addActionListener(e -> System.out.println(checkBoxAcceptTerm.isSelected()));
+        this.add(checkBoxAcceptTerm);
+
+        checkBoxAcceptTerm1 = new JCheckBox("Acepto términos y condiciones");
+        checkBoxAcceptTerm1.setBounds(10, 100, 200, 30);
+        checkBoxAcceptTerm1.addActionListener(e -> System.out.println(checkBoxAcceptTerm.isSelected()));
+        this.add(checkBoxAcceptTerm1);
+
+        checkBoxAcceptTerm2 = new JCheckBox("Acepto términos y condiciones");
+        checkBoxAcceptTerm2.setBounds(10, 150, 200, 30);
+        checkBoxAcceptTerm2.addActionListener(e -> System.out.println(checkBoxAcceptTerm.isSelected()));
+        this.add(checkBoxAcceptTerm2);
+
+        buttonGroup = new ButtonGroup();
+
+        jRadioButtonOption1 = new JRadioButton("Opción 1");
+        jRadioButtonOption1.setBounds(10, 200, 200, 30);
+        jRadioButtonOption1.setActionCommand("OPTION1");
+        jRadioButtonOption1.addActionListener(e -> System.out.println("Opción 1"));
+        this.add(jRadioButtonOption1);
+        buttonGroup.add(jRadioButtonOption1);
+
+        jRadioButtonOption2 = new JRadioButton("Opción 2");
+        jRadioButtonOption2.setBounds(10, 250, 200, 30);
+        jRadioButtonOption2.addActionListener(e -> System.out.println("Opción 2"));
+        this.add(jRadioButtonOption2);
+        buttonGroup.add(jRadioButtonOption2);
+
+        jRadioButtonOption3 = new JRadioButton("Opción 3");
+        jRadioButtonOption3.setBounds(10, 300, 200, 30);
+        jRadioButtonOption3.addActionListener(e -> System.out.println("Opción 3"));
+        this.add(jRadioButtonOption3);
+        buttonGroup.add(jRadioButtonOption3);
     }
 }
